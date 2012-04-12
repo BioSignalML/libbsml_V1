81a4a2aefa7518ef457dd3c89f14b2e7df3b69e7
Dave Brooks <dave@bcs.co.nz>
2012-04-12 17:55:01 +1200

    Use new function names, etc, in web sockets test program.

--- src/tests/websocket.c
+++ src/tests/websocket.c
@@ -5,44 +5,31 @@
 
 
 
-#include "bsml-stream.h"
+#include "bsml_stream.h"
 
 #define REPOSITORY_STREAM "/stream/data/"
-
-
-const char *string_copy(const char *s)
-/*==================================*/
-{
-  if (s) {
-    int l = strlen(s) + 1 ;
-    char *t = malloc(l) ;
-    memcpy(t, s, l) ;
-    return t ;
-    }
-  else return NULL ;
-  }
 
 
 int main(void)
 /*==========*/
 {
 
-  stream_initialise() ;
+  bsml_stream_initialise() ;
 
-  stream_data *strm = stream_data_request("localhost", 80,
-    REPOSITORY_STREAM, "http://devel.biosignalml.org/test/sinewave6", 0.0, 1.0) ;
+  bsml_stream_data *strm = bsml_stream_data_request("localhost", 80,
+    REPOSITORY_STREAM, "http://devel.biosignalml.org/recording/test/sw4", 0.0, 1.0) ;
 
-  stream_block *sb ;
+  bsml_stream_block *sb ;
   
-  printf("%d, %lx\n", strm->state, strm->block) ;
+  printf("%d, %lx\n", strm->state, (long)strm->block) ;
 
-  while ((sb = stream_data_read(strm)) != NULL) {
+  while ((sb = bsml_stream_data_read(strm)) != NULL) {
     printf("Block %c: %d\n", sb->type, sb->length) ;
-    if      (sb->type == ERROR_BLOCK) {
+    if      (sb->type == BSML_STREAM_ERROR_BLOCK) {
       printf("ERROR: %-*s\n", sb->length, sb->content) ;
       }
 
-    else if (sb->type == DATA_BLOCK) {
+    else if (sb->type == BSML_STREAM_DATA_BLOCK) {
       double *dp = (double *)sb->content ;
       int l = sb->length/sizeof(double) ;
 
@@ -60,17 +47,17 @@ int main(void)
 //      memcpy(buffer, (double *)sb->content, sb->length*sizeof(dtype)) ;
 
       }
-    stream_free_block(sb) ;
+    bsml_stream_free_block(sb) ;
     }
 
-  if (strm->error != STREAM_ERROR_NONE)  // Always check for errors...
-    printf("ERROR %d: %s\n", strm->error, stream_error_text(strm->error)) ;
+  if (strm->error != BSML_STREAM_ERROR_NONE)  // Always check for errors...
+    printf("ERROR %d: %s\n", strm->error, bsml_stream_error_text(strm->error)) ;
 
-  printf("%d, %lx\n", strm->state, strm->block) ;
+  printf("%d, %lx\n", strm->state, (long)strm->block) ;
 
-  stream_free_data(strm) ;
+  bsml_stream_free_data(strm) ;
 
 
-  stream_finish() ;
+  bsml_stream_finish() ;
 
   }
