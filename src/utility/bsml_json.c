ca1367cc1b4d99e8c711f0e634a2f61054172aba
Dave Brooks <dave@bcs.co.nz>
2012-04-12 12:35:54 +1200

    Add our utility functions for getting values from JSON.

---
+++ src/utility/bsml_json.c
@@ -0,0 +1,34 @@
+/*****************************************************
+ *
+ *  BioSignalML API
+ *
+ *  Copyright (c) 2010-2012  David Brooks
+ *
+ *  $ID$
+ *
+ *****************************************************/
+
+#include "bsml_internal.h"
+#include "utility/bsml_json.h"
+
+const char *bsml_json_as_string(json_t *obj, const char *key)
+/*=========================================================*/
+{
+  json_t *entry = json_object_get(obj, key) ;
+  if (entry && json_is_string(entry)) return json_string_value(entry) ;
+  else {
+    bsml_log_error("Expected a string for '%s' JSON key\n", key) ;
+    return NULL ;
+    }
+  }
+
+double bsml_json_as_number(json_t *obj, const char *key)
+/*====================================================*/
+{
+  json_t *entry = json_object_get(obj, key) ;
+  if (entry && json_is_number(entry)) return json_number_value(entry) ;
+  else {
+    bsml_log_error("Expected a number for '%s' JSON key\n", key) ;
+    return 0.0 ;
+    }
+  }
---
+++ src/utility/bsml_json.h
@@ -0,0 +1,43 @@
+/*****************************************************
+ *
+ *  BioSignalML API
+ *
+ *  Copyright (c) 2010-201  David Brooks
+ *
+ *  $ID$
+ *
+ *****************************************************/
+/** @file
+  * Additional operations on C strings.
+  */
+
+#ifndef _BSML_JSON_H
+#define _BSML_JSON_H
+
+#include <jansson.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Get a string value from a JSON object.
+  * @param obj A JSON object.
+  * @param key The key of the string value.
+  * @return The string value if the key exists and refers to a string, otherwise an error message
+  * is written to 'stderr' and NULL is returned.
+  */
+const char *bsml_json_as_string(json_t *obj, const char *key) ;
+
+/** Get a numeric value from a JSON object.
+  * @param obj A JSON object.
+  * @param key The key of the numeric value.
+  * @return The numeric value if the key exists and refers to a number, otherwise an error message
+  * is written to 'stderr' and 0.0 is returned.
+  */
+double bsml_json_as_number(json_t *obj, const char *key) ;
+
+#ifdef __cplusplus
+  } ;
+#endif
+
+#endif
