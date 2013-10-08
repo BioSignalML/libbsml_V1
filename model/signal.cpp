6025d665d2288499f658e76787c05a53caa92767
David Brooks <dave@bcs.co.nz>
2013-10-09 12:51:00 +1300

    Use correct predicate for a signal's recording.

--- model/signal.cpp
+++ model/signal.cpp
@@ -8,7 +8,7 @@ void bsml::Signal::set_recording(bsml::Recording *recording)
 /*--------------------------------------------------------*/
 {
   recording_ = recording ;
-  rdfmap.push_back(new rdf::Mapping<rdf::Resource>(rdf::RDF::type, recording->get_resource())) ;
+  rdfmap.push_back(new rdf::Mapping<rdf::Resource>(bsml::BSML::recording, recording->get_resource())) ;
   }
 
 
