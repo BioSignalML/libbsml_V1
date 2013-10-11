e46e1f79ad0991fa968be2b803813e2156653cbe
David Brooks <dave@bcs.co.nz>
2013-10-11 13:40:50 +1300

    Start using real dates and times.

--- model/datetime.cpp
+++ model/datetime.cpp
@@ -1,19 +1,34 @@
+#include <string>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+
 #include "datetime.h"
 #include "rdfnames.h"
 
+using namespace boost::posix_time ;
 
 using namespace bsml ;
 
 
 Datetime::Datetime(void)
 /*--------------------*/
-: rdf::Literal("", rdf::XSD::dateTime)
+: rdf::Literal("", rdf::XSD::dateTime),
+  datetime(microsec_clock::universal_time())
 {
+  }
+
+std::string Datetime::as_iso_string(void)
+/*-------------------------------------*/
+{
+  boost::gregorian::date ymd = datetime.date() ;
+  time_duration hms = datetime.time_of_day() ;
+  return boost::gregorian::to_iso_extended_string(ymd) + "T" + to_simple_string(hms) + "Z" ;
   }
 
 
 Duration::Duration(void)
 /*--------------------*/
-: rdf::Literal("", rdf::XSD::dayTimeDuration)
+: rdf::Literal("", rdf::XSD::dayTimeDuration),
+  duration(0, 0, 0, 0)
 {
   }
--- model/datetime.h
+++ model/datetime.h
@@ -1,6 +1,9 @@
 #ifndef _BSML_DATETIME_H
 #define _BSML_DATETIME_H
 
+#include <string>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+
 #include "rdf.h"
 
 
@@ -9,13 +12,18 @@ namespace bsml {
   class Datetime : public rdf::Literal
   /*================================*/
   {
+   private:
+    boost::posix_time::ptime datetime ;
    public:
     Datetime(void) ;
+    std::string as_iso_string(void) ;
     } ;
 
   class Duration : public rdf::Literal
   /*================================*/
   {
+   private:
+    boost::posix_time::time_duration duration ;
    public:
     Duration(void) ;
     } ;
