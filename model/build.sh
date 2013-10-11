ef9428dda954df222a48e05e6af55339197eec62
David Brooks <dave@bcs.co.nz>
2013-10-11 13:40:04 +1300

    Use a script for building (will become a Makefile…)

---
+++ model/build.sh
@@ -0,0 +1,2 @@
+echo c++ -o test  test.cpp object.cpp recording.cpp signal.cpp  datetime.cpp rdf.cpp  -lrdf -lboost_date_time
+c++ -o test  test.cpp object.cpp recording.cpp signal.cpp datetime.cpp rdf.cpp  -lrdf -lboost_date_time
