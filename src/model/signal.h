a1d274847b1c40055d3811283a11bd13278fe853
David Brooks <dave@bcs.co.nz>
2013-10-25 17:39:23 +1300

    Remove note to self...

--- src/model/signal.h
+++ src/model/signal.h
@@ -34,41 +34,6 @@ namespace bsml {
     } ;
 
 
-/*
-  metaclass = BSML.Signal     #: :attr:`.BSML.Signal`
-
-  attributes = ['recording', 'units', 'transducer', 'filter', '_rate',  '_period', 'clock',
-                'minFrequency', 'maxFrequency', 'minValue', 'maxValue',
-                'index', 'signaltype', 'offset', '_duration',
-               ]
-  '''Generic attributes of a Signal.'''
-
-  mapping = { 'recording':    PropertyMap(BSML.recording, to_rdf=PropertyMap.get_uri),
-              'units':        PropertyMap(BSML.units, to_rdf=PropertyMap.get_uri),
-              'sensor':       PropertyMap(BSML.sensor),
-              'filter':       PropertyMap(BSML.preFilter),
-              '_rate':        PropertyMap(BSML.rate, XSD.double),
-              '_period':      PropertyMap(BSML.period, XSD.double),
-
-              'clock':        PropertyMap(BSML.clock, to_rdf=PropertyMap.get_uri, subelement=True),
-
-              'minFrequency': PropertyMap(BSML.minFrequency, XSD.double),
-              'maxFrequency': PropertyMap(BSML.maxFrequency, XSD.double),
-              'minValue':     PropertyMap(BSML.minValue, XSD.double),
-              'maxValue':     PropertyMap(BSML.maxValue, XSD.double),
-              'dataBits':     PropertyMap(BSML.dataBits, XSD.integer),
-              'signaltype':   PropertyMap(BSML.signalType),
-
-
-              'offset':       PropertyMap(BSML.offset, XSD.dayTimeDuration,
-                                          utils.seconds_to_isoduration,
-                                          utils.isoduration_to_seconds),
-              '_duration':    PropertyMap(DCT.extent, XSD.dayTimeDuration,
-                                          utils.seconds_to_isoduration,
-                                          utils.isoduration_to_seconds),
-            }
-
-*/
   template <class SIGNAL>
   class SignalGroup : public std::vector<SIGNAL *>
   /*============================================*/
